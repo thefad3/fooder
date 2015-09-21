2af81eac2f8386da64d6a43e2f07ae4fc4687572
Chris Lynch <chrislynch@Chriss-MacBook-Pro.local>
2015-09-21 19:00:33 -0400

    Fixed searing json callback

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,13 +4,11 @@ function randomString(length, chars) {
   var result = '';
   for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
   return result;
-}
 
+}
 angular.module('fooderApp')
-  .controller('authPage', function($scope,$firebaseArray, Auth, $http, Ref, $firebaseObject, $timeout, $location){
-    $scope.animationsEnabled = false;
-    $scope.loadingFalse=1;
-    $scope.businesses=1;
+  .controller('authPage', function($scope,$firebaseArray, Auth, $http, Ref, $firebaseObject, $timeout, $location, Yelp){
+    $scope.callbBackId=0;
     $scope.business_index=0;
     var _authDataReturned = Auth.$getAuth();
     var fb = new Firebase('https://coderr.firebaseio.com/' + _authDataReturned.uid);
@@ -23,26 +21,10 @@ angular.module('fooderApp')
     }
 
     var callBack = function (location) {
+      //Location data for when Page loads
       $scope.locationData = location.city + ',' + location.region;
-      console.log(location);
-      $scope.loadSearch = {
-        location: location.city + ',' + location.region,
-        term: 'food'
-      };
 
-      $scope.map = {center: {latitude: location.latitude, longitude: location.longitude }, zoom: 6 };
-      $scope.options = {scrollwheel: false};
-      $scope.coordsUpdates = 0;
-      $scope.dynamicMoveCtr = 0;
-      $scope.marker = {
-        coords: {
-          latitude: location.latitude,
-          longitude: location.longitude
-        },
-        options: { draggable: false }
-        };
-
-
+      //Google maps location generator and map ganerator
       $scope.showPosition = function(position) {
         var latlon = position.latitude + "," + position.longitude;
         var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
@@ -50,132 +32,60 @@ angular.module('fooderApp')
         document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
       };
 
-
-      $scope.searchFunction = function (name) {
-        var method = 'GET',
-          url = 'https://api.yelp.com/v2/search/',
-          params = {
-            callback: 'angular.callbacks._0',
-            location: name.location,
-            oauth_consumer_key: '44dGAgblwMC4eiapEgv2Eg',
-            oauth_token: 'a85eWTlMIhs34Ehs-z9ZmPxrbrVPAMnv',
-            oauth_signature_method: "HMAC-SHA1",
-            oauth_timestamp: new Date().getTime(),
-            oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-            term: name.term
-          },
-          consumerSecret = '7cLJ2tyXnPmdvWDakkcyRTs4qYY',
-          tokenSecret = '9qxs-Xd-d11WrjGd_96yQB-raQY',
-          signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encodeSignature: false});
-          params['oauth_signature'] = signature;
-          $http.jsonp(url, {params: params}).success(function(data){
-          $scope.viewData = data.businesses;
-            console.log(data);
-          }).error(function(data, status, headers, config) {
-            console.log(data);
-        });
+      var _searchLoad = {
+        location: $scope.locationData,
+        term: 'food',
+        callbackId: 0
       };
-      $scope.searchFunction($scope.loadSearch);
+      Yelp.yelpSearch(_searchLoad, function(data){
+        $scope.viewData = data.businesses;
+      })
     };
 
     NoGPS.getLocation(callBack);
 
-
-
     $scope.comment = function(x, a){
-
       var fb = new Firebase('https://coderr.firebaseio.com/' + x.id);
-
       var arry = $firebaseArray(fb);
-      console.log(arry);
       arry.$add({ comments: a, pid: _authDataReturned.uid }).then(function(data){
-
           console.log('Success, posted!');
-
       });
-
     };
 
-
-
     $scope.like = function(index){
-
-
       $scope.next();
     };
     $scope.dislike = function(index){
-
-
-
       $scope.next();
     };
 
 
     $scope.next = function(){
       if($scope.business_index >= $scope.viewData.length -1){
-        $scope.business_index =0;
+        $scope.business_index=0;
       }else{
         $scope.business_index ++;
       }
     };
 
-    $scope.logout = function(){
-      ref.unauth();
-      $location.path('/#/');
-    };
 
     $scope.search = function() {
-      $scope.loading=1;
       $scope.viewData=[];
-
+      $scope.callbBackId++;
       var _search = {
         location: $scope.locationData,
-        term: $scope.term
+        term: $scope.term,
+        callbackId: $scope.callbBackId
       };
-
-      $scope.navSearch = function (name) {
-
-        var method = 'GET',
-          url = 'https://api.yelp.com/v2/search/',
-          params = {
-            callback: 'angular.callbacks._0',
-            location: name.location,
-            oauth_consumer_key: '44dGAgblwMC4eiapEgv2Eg',
-            oauth_token: 'a85eWTlMIhs34Ehs-z9ZmPxrbrVPAMnv',
-            oauth_signature_method: "HMAC-SHA1",
-            oauth_timestamp: new Date().getTime(),
-            oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzA'),
-            term: name.term
-          };
-          var consumerSecret = '7cLJ2tyXnPmdvWDakkcyRTs4qYY',
-          tokenSecret = '9qxs-Xd-d11WrjGd_96yQB-raQY',
-          signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encodeSignature: false});
-        params['oauth_signature'] = signature;
-        $http.jsonp(url, {params:params}).success(function(data){
-          $scope.viewData = data.businesses;
-          console.log(data);
-        }).error(function(data, status, headers, config) {
-          console.log(data);
-        });
-      };
-
-      $scope.navSearch(_search);
-
+      Yelp.yelpSearch(_search, function(data){
+        $scope.viewData = data.businesses;
+      });
 
     };
-
-
-
-
-
-
 
 
   })
-
   .controller('MainCtrl', function ($scope, Auth, $location, Ref) {
-
-
       $scope.signup = function(){
         Auth.$createUser({
           email: $scope.register.email,
@@ -184,7 +94,6 @@ angular.module('fooderApp')
 
           var usersRef = Ref.child("/"+userData.uid);
           usersRef.set(userData);
-            console.log("User " + userData.uid + " created successfully!");
         })
       };
 
@@ -204,4 +113,27 @@ angular.module('fooderApp')
       });
     };
 
-  });
+  })
+  .factory("Yelp", function($http) {
+    return {
+      "yelpSearch": function (name, callback) {
+        var method = 'GET',
+          url = 'https://api.yelp.com/v2/search/',
+          params = {
+            callback: 'angular.callbacks._'+name.callbackId,
+            location: name.location,
+            oauth_consumer_key: '44dGAgblwMC4eiapEgv2Eg',
+            oauth_token: 'a85eWTlMIhs34Ehs-z9ZmPxrbrVPAMnv',
+            oauth_signature_method: "HMAC-SHA1",
+            oauth_timestamp: new Date().getTime(),
+            oauth_nonce: randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzA'),
+            term: name.term
+          };
+        var consumerSecret = '7cLJ2tyXnPmdvWDakkcyRTs4qYY',
+          tokenSecret = '9qxs-Xd-d11WrjGd_96yQB-raQY',
+          signature = oauthSignature.generate(method, url, params, consumerSecret, tokenSecret, {encodeSignature: false});
+        params['oauth_signature'] = signature;
+        $http.jsonp(url, {params: params}).success(callback);
+      }
+    }
+  })
